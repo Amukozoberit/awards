0c9c0b41e63a271e68e744d7ae1fdff38d357cd5
Amukozoberit <mwasheb@gmail.com>
2021-09-18 09:00:51 +0300

    created serializer form

--- projectsAwards/forms.py
+++ projectsAwards/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Project
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields=['profile_pic','bio','contact']
+
+class ProjectsForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields=['title','landing_page','description','link']
