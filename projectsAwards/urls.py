4b24eecc4bdc9bff2d258f4c2347390547b2fc3b
Amukozoberit <mwasheb@gmail.com>
2021-09-18 09:02:30 +0300

    created urls

--- projectsAwards/urls.py
+++ projectsAwards/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
  url(r'^$',views.home,name='indexpage'),
+url(r'^api/profiles/$', views.ProfileList.as_view()),
+url(r'^api/projects/$', views.ProjectList.as_view()),
+url('search/', views.search_results, name='search_results'),
+url('single_project/(\d+)', views.single_project, name='single_project'),
+url('profile/(\d+)', views.single_profile, name='profile')
 #  url(r'profile',views.update_profile,name='profilepage')]
 ]
 
