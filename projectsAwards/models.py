fd5bcb86cc44de8867cede5e0746c6f214181f28
Amukozoberit <mwasheb@gmail.com>
2021-09-18 09:01:28 +0300

    added model view

--- projectsAwards/models.py
+++ projectsAwards/models.py
@@ -4,12 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 from django.db.models.signals import post_save
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Avg  
+
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic=models.ImageField(upload_to='profile_pics/', blank=True)
     bio=models.TextField(max_length='50',blank=True)
-    # projects=models.ForeignKey()
+    # projects=models.On
     contact=models.TextField(max_length=50,blank=True)
 
 
@@ -31,16 +33,29 @@ class Project(models.Model):
     link=models.URLField(max_length=200,blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     users=models.ForeignKey(User,on_delete=models.CASCADE)
-
-
+    # rates=OneToMany(to=Rater)
+    @classmethod
+    def search_by_title(cls,search_term):
+        news=cls.objects.filter(title__icontains=search_term)
+        return news
+# class Languages(models.Model):
+#     name=models.TextField(max_length=4)
+#     perc=models.FloatField()
 class Rater(models.Model):
     users=models.ForeignKey(User,on_delete=models.CASCADE)
     projects=models.ForeignKey(Project,on_delete=models.CASCADE)
-    design=models.IntegerField()
-    usability=models.IntegerField()
-    content=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    average=models.FloatField(blank=True)
+    design=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    usability=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    content=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    # average=models.FloatField(blank=True)
+    # @classmethod
+    # def get_avg(cls):
+    #     Rater.objects.aggregate(Avg('average'))
 
 
+# class ProfileMerch(models.Model):
+#     user=models.CharField(max_length=40)
+#     profile_pic=models.ImageField(upload_to='profile_pics/')
 
-
+#     bio=models.TextField(max_length=40)
+#     contact=models.TextField(max_length=40)
