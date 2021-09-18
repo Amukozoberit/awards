3ccd6edc3578a9b70c6478e3a4a2306b8af296a5
Amukozoberit <mwasheb@gmail.com>
2021-09-18 16:41:06 +0300

    added urls

--- projectsAwards/urls.py
+++ projectsAwards/urls.py
@@ -9,7 +9,12 @@ url(r'^api/profiles/$', views.ProfileList.as_view()),
 url(r'^api/projects/$', views.ProjectList.as_view()),
 url('search/', views.search_results, name='search_results'),
 url('single_project/(\d+)', views.single_project, name='single_project'),
-url('profile/(\d+)', views.single_profile, name='profile')
+url('profile/(\d+)', views.single_profile, name='profile'),
+url('addrate/(\d+)', views.addrate, name='addrate'),
+url('editprofile/', views.update_profile, name='editprofile'),
+url('addproject/', views.addproject, name='addproject'),
+url('allproject/', views.allprojects, name='allproject'),
+# url('siteofday/', views.siteofDay, name='siteofDay')
 #  url(r'profile',views.update_profile,name='profilepage')]
 ]
 
